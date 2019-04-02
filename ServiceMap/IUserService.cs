dd5c455164f113d220e0d63958b9f8424bf62db0
elyes <elyesjrad95@gmail.com>
2019-04-02 17:12:55 +0200

    userservice

---
+++ ServiceMap/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using DomainMap.Entities;
+using ServicePattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceMap
+{
+ public   interface IUserService: IServicePattern<User>
+    {
+        Dictionary<String, int> userdate();
+    }
+}
---
+++ ServiceMap/UserService.cs
@@ -0,0 +1,37 @@
+﻿
+using DataMap.Infrastructure;
+using DomainMap.Entities;
+using ServicePattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceMap
+{
+  public  class UserService : ServicePattern<User>, IUserService
+    {
+        private static IDatabaseFactory databaseFactory = new DatabaseFactory();
+        private static IUnitOfWork unit = new UnitOfWork(databaseFactory);
+        public UserService():base(unit)
+        {
+
+        }
+        public enum mois {  janvier=1,fevrier=2, mars = 3, avril=4, mai=5,juin=6,juillet=7,aout=8,septembre=9,october=10,novembre=11,decembre=12 }
+
+        public Dictionary<String,int> userdate()
+        {
+            Dictionary<String, int> dicuser = new Dictionary<string, int>();
+            foreach (mois item in Enum.GetValues(typeof(mois)) )
+            {
+             //   var o = GetMany(t => (int)item == (t.dateEntree.Month)).Count();
+              //  int s = GetMany(t => (int)item == (t.LockoutEndDateUtc.Value.Month)).Count();
+              //  dicuser.Add(item.ToString(), s);
+            }
+            return dicuser;
+        }
+       
+        
+    }
+}
