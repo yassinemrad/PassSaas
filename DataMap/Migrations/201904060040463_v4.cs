68ba82e6e7b06a1ab519f899bae3f04853136baf
Unknown <yassin.mrad@yahoo.fr>
2019-04-06 02:36:34 +0100

    temp

--- DataMap/Migrations/201904060040463_v4.cs
+++ DataMap/Migrations/201904060040463_v4.cs
@@ -3,7 +3,7 @@ namespace DataMap.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class v1 : DbMigration
+    public partial class v4 : DbMigration
     {
         public override void Up()
         {
