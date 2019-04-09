78ebdaee6c3aaa2a2162aab8b9e7b4239b5a0f87
sabrine <sabrine.mzeh@esprit.tn>
2019-04-09 18:16:57 +0100

    forum

---
+++ WebMap/Models/CommentModels.cs
@@ -0,0 +1,19 @@
+﻿using DomainMap.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebMap.Models
+{
+    public class CommentModels
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ChampComment { get; set; }
+        public User user { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+        public Forum forum { get; set; }
+    }
+}
---
+++ WebMap/Models/ForumModels.cs
@@ -0,0 +1,26 @@
+﻿using DomainMap.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMap.Models
+{
+    public enum Categorie { DotNet, JEE, C, python }
+    public class ForumModels
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime Date { get; set; }
+        public string Author { get; set; }
+        [EnumDataType(typeof(Categorie))]
+        public Categorie Categorie { get; set; }
+        [UIHint("tinymce_jquery_full"),AllowHtml] 
+        public string Contenu { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
