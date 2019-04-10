013665fb9141f0db88f802d5af920445e4e63ad4
marwa <marwa.gaaloul@esprit.tn>
2019-04-10 20:16:25 +0100

    marwaproject

--- ServiceMap/ProjetService.cs
+++ ServiceMap/ProjetService.cs
@@ -4,6 +4,7 @@ using ServicePattern;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -75,8 +76,66 @@ namespace ServiceMap
             var z = (double)aut / x * 100;
             return z;
         }
+
+
+
+
+        public IEnumerable<Projet> listprojet()
+        {
+            var v = GetAll();
+
+            return v;
+
+        }
+        public IEnumerable<Projet> getNonDelivredProject() //obtenir un projet non livré
+        {
+            return unit.getRepository<Projet>().GetMany(p => p.endDate > DateTime.Now).ToList();
+
+        }
+        public IEnumerable<Projet> getDelivredProject() //obtenir un projet livré
+        {
+            return unit.getRepository<Projet>().GetMany(p => p.endDate <= DateTime.Now).ToList();
+        }
+        public IEnumerable<Projet> getProjectInDate(DateTime start, DateTime end) //Obtenir la date du projet
+        {
+            return unit.getRepository<Projet>().GetMany(p => p.startDate >= start && p.endDate <= end).ToList();
+        }
+        public IEnumerable<Projet> getMostExpensiveProject()             //obtenir le projet le plus cher
+
+        {
+            return unit.getRepository<Projet>().GetAll().OrderByDescending(p => p.Budget).Take(3).ToList();
+        }
+        public IEnumerable<ProjectBudget> Get6MostCostlyProjectsForATeamLeader(TeamLeader e)
+        //Obtenez 6 projets les plus chers pour un chef d'équipe
+        {
+            return GetAll()
+
+
+                .ToList()
+                .OrderByDescending(GP => GP.Budget)
+                .Take(6)
+                .Select(GP => new ProjectBudget
+                {
+                    ProjectName = GP.name,
+                    Budget = GP.Budget
+                }).ToList();
+        }
+
+        public float progressProject(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    [DataContract(IsReference = true)]
+    public class ProjectBudget
+    {
+        [DataMember]
+        public string ProjectName { get; set; }
+        [DataMember]
+        public float Budget { get; set; }
     }
 }
+
 
     
 
---
+++ WebMap/Controllers/ProjetApiController.cs
@@ -0,0 +1,144 @@
+﻿using DomainMap.Entities;
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Data.Entity;
+using ServiceMap;
+
+namespace WebMap.Controllers
+{
+    public class ProjetApiController : ApiController
+    {
+        IProjetService Ps = new ProjetService();
+        [Route("api/ProjetApiController/GetDelivredProjects")]
+        [HttpGet]
+        public HttpResponseMessage GetDelivredProjects()
+        {
+            var Projects = Ps.getDelivredProject();
+            List<Projet> listp = new List<Projet>();
+            foreach (var item in Projects)
+            {
+                listp.Add(new Projet
+                { id = item.id,
+                    name = item.name,
+                    startDate = item.startDate,
+                    endDate = item.endDate,
+                    Budget = item.Budget,
+                    cathegory = item.cathegory
+
+                });
+
+
+            }
+            if (Projects.Count() == 0)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, listp);
+        }
+        
+        public IQueryable<Projet> Get()
+        {
+            List<Projet> list = Ps.GetAll().ToList();
+            return list.AsQueryable();
+        }
+
+        
+
+
+
+
+
+        // POST: api/ProjetApi
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/ProjetApi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ProjetApi/5
+        public void Delete(int id)
+        {
+        }
+        [Route("api/ProjetApiController/GetMostExpensive")]  /*//http://localhost:21514/api/ProjetApiController/GetMostExpensive*/
+        [HttpGet]
+        public HttpResponseMessage GetMostExpensive()
+        {
+            var Projects = Ps.getMostExpensiveProject();
+            List<Projet> listp = new List<Projet>();
+            foreach (var item in Projects)
+            {
+                listp.Add(new Projet
+                {
+                    name = item.name,
+                    startDate = item.startDate,
+                    endDate = item.endDate,
+                    Budget = item.Budget,
+                    cathegory = item.cathegory
+
+                });
+
+
+            }
+            if (Projects.Count() == 0)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, listp);
+
+        }
+        [Route("api/ProjetApiController/delete/{id}")]
+        [HttpGet]
+        public void DeleteProvider(int id)
+        {
+           Projet p = Ps.GetById(id);
+            if (p == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            Ps.Delete(p);
+            Ps.Commit();
+        }
+        [Route("api/ProjetApiController/Details/{id}")]
+        [HttpGet]
+        public HttpResponseMessage Details(int id)
+        {
+            Projet p = Ps.GetById(id);
+            Projet pvm = new Projet()
+
+            {
+                
+
+                name = p.name,
+                description = p.description,
+                Photo=p.Photo,
+                startDate = p.startDate,
+                endDate = p.endDate,
+                Budget = p.Budget,
+
+
+                cathegory = p.cathegory
+            };
+            if (p == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, pvm);
+
+        }
+
+    }
+
+}
+
+
